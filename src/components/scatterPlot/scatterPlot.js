e0f046fc25d37681e368228bb4fafef23e64852c
n-inokawa <inkwmwmw@gmail.com>
2020-01-11 23:57:57 +0900

    Add scatterPlot

---
+++ src/components/scatterPlot/scatterPlot.js
@@ -0,0 +1,60 @@
+import * as d3 from 'd3';
+import setAxis from '../../utils/setAxis';
+
+/**
+ * 散布図
+ * @param {*} elem
+ * @param {*} data
+ * @param {*} xParam
+ * @param {*} yParam
+ */
+function init(elem, data, xParam, yParam, name) {
+  // svg領域設定
+  const svgWidth = 600;
+  const svgHeight = 400;
+  const svg = d3.select(elem)
+    .append('svg')
+    .attr('width', svgWidth)
+    .attr('height', svgHeight);
+
+  // グラフ領域設定
+  const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+  const width = svgWidth - margin.right - margin.left;
+  const height = svgHeight - margin.top - margin.bottom;
+  const graph = svg
+    .append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+
+  // scale設定
+  const xScale = d3.scaleLinear().range([0, width]);
+  const yScale = d3.scaleLinear().range([height, 0]);
+
+  update(data);
+
+  function update(data) {
+    // ドメイン設定
+    xScale.domain(d3.extent(data, d => d[xParam.key]));
+    yScale.domain([0, d3.max(data, d => d[yParam.key])]);
+
+    // 軸の作成
+    setAxis(graph, xScale, yScale, xParam.name, yParam.name, width, height, margin);
+
+    // 散布図の作成
+    const svgData = graph.selectAll('circle')
+      .data(data, d => d[name]);
+
+    svgData.enter()
+      .append('circle')
+      .merge(svgData)
+      .transition()
+      .attr('r', 3)
+      .attr('cx', d => xScale(d[xParam.key]))
+      .attr('cy', d => yScale(d[yParam.key]))
+      .attr('class', 'plot');
+
+    svgData.exit()
+      .remove();
+  }
+}
+
+export default init;
---
+++ src/components/scatterPlot/scatterPlot.stories.js
@@ -0,0 +1,35 @@
+
+import { withCssResources } from '@storybook/addon-cssresources';
+import init from './scatterPlot.js';
+import getRangedData from '../../utils/getRangedData.js';
+
+export default {
+  title: 'scatterPlot',
+  decorators: [withCssResources],
+  parameters: {
+    cssresources: [{
+      id: `scatterPlot`,
+      code: `<style>
+      .plot {
+        fill: steelblue;
+      }
+      </style>`,
+      picked: true,
+    },
+    ],
+  },
+};
+
+export const normal = () => {
+  const div = document.createElement('div');
+  init(div, getRangedData(1000),
+    {
+      key: 'name',
+      name: '名前'
+    },
+    {
+      key: 'value',
+      name: '値'
+    });
+  return div;
+};
