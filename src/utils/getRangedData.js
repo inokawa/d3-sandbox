0ff9404de2197f091df8750ad9bb0625c86d849b
n-inokawa <inkwmwmw@gmail.com>
2020-01-11 23:55:29 +0900

    Add util functions

---
+++ src/utils/getRangedData.js
@@ -0,0 +1,13 @@
+import * as d3 from 'd3';
+
+export default function getRangedData(max) {
+  const list = [];
+  const arr = d3.range(0, Math.ceil(Math.random() * 30));
+  for (let i = 0; i < arr.length; i++) {
+    list.push({
+      name: arr[i] * 10,
+      value: Math.floor(Math.random() * Math.random() * max)
+    });
+  }
+  return list;
+}
