6a93c7e5794f00db2bfd5282a128a4c927b35e97
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-23 22:52:43 +0900

    Add globe story

--- src/components/worldMap/index.js
+++ src/components/worldMap/index.js
@@ -1,21 +1,22 @@
-import * as d3 from 'd3';
+import * as d3 from "d3";
 
-function init(elem, data) {
+function mapInit(elem, data) {
   const margin = { top: 10, right: 20, bottom: 10, left: 20 };
   const width = 1000;
   const height = 600;
-  const svg = d3.select(elem)
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
-  const mapGroup = svg.append('g');
+  const svg = d3
+    .select(elem)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+  const mapGroup = svg.append("g");
 
-  const projection = d3.geoMercator()
+  const projection = d3
+    .geoMercator()
     // .scale(340)
     // .center([33, 100])
     .translate([width / 2, height / 2]);
-  const path = d3.geoPath()
-    .projection(projection);
+  const path = d3.geoPath().projection(projection);
 
   // const zoom = d3.zoom()
   //   .scaleExtent([1, 40])
@@ -25,44 +26,129 @@ function init(elem, data) {
 
   let active;
 
-  mapGroup.selectAll('path')
+  mapGroup
+    .selectAll("path")
     .data(data)
     .enter()
-    .append('path')
-    .attr('d', path)
-    .attr('class', 'area')
-    .on('mouseenter', function(d) {
-      d3.select(this).classed('selected', true);
-      mapGroup.append('text')
-        .classed('tooltip', true)
-        .style('text-anchor', 'middle')
-        .attr('x', 100)
-        .attr('y', 300)
+    .append("path")
+    .attr("d", path)
+    .attr("class", "area")
+    .on("mouseenter", function (d) {
+      d3.select(this).classed("selected", true);
+      mapGroup
+        .append("text")
+        .classed("tooltip", true)
+        .style("text-anchor", "middle")
+        .attr("x", 100)
+        .attr("y", 300)
         .text(d.properties.name);
     })
-    .on('mouseleave', function(d) {
-      d3.select(this).classed('selected', false);
-      d3.selectAll('.tooltip').remove();
+    .on("mouseleave", function (d) {
+      d3.select(this).classed("selected", false);
+      d3.selectAll(".tooltip").remove();
     })
-    .on('click', function(d) {
+    .on("click", function (d) {
       if (active === d) return reset();
-      mapGroup.selectAll('.active').classed('active', false);
-      d3.select('#' + d.properties.name).classed('active', active = d);
+      mapGroup.selectAll(".active").classed("active", false);
+      d3.select("#" + d.properties.name).classed("active", (active = d));
 
       const b = path.bounds(d);
 
-      mapGroup.transition().duration(700).attr('transform',
-        'translate(' + projection.translate() + ')'
-        + 'scale(' + .95 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ')'
-        + 'translate(' + -(b[1][0] + b[0][0]) / 2 + ',' + -(b[1][1] + b[0][1]) / 2 + ')');
+      mapGroup
+        .transition()
+        .duration(700)
+        .attr(
+          "transform",
+          "translate(" +
+            projection.translate() +
+            ")" +
+            "scale(" +
+            0.95 /
+              Math.max(
+                (b[1][0] - b[0][0]) / width,
+                (b[1][1] - b[0][1]) / height
+              ) +
+            ")" +
+            "translate(" +
+            -(b[1][0] + b[0][0]) / 2 +
+            "," +
+            -(b[1][1] + b[0][1]) / 2 +
+            ")"
+        );
     });
 
-
   function reset() {
-    mapGroup.selectAll('.active').classed('active', active = false);
-    mapGroup.transition().duration(700).attr('transform', '');
+    mapGroup.selectAll(".active").classed("active", (active = false));
+    mapGroup.transition().duration(700).attr("transform", "");
   }
-
 }
 
-export default init;
+function globeInit(elem, data) {
+  const width = 960;
+  const height = 500;
+  const config = {
+    speed: 0.005,
+    verticalTilt: -30,
+    horizontalTilt: 0,
+  };
+
+  const svg = d3
+    .select(elem)
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+  const projection = d3.geoOrthographic();
+  const path = d3.geoPath().projection(projection);
+
+  drawGraticule();
+
+  svg
+    .selectAll(".area")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("class", "area")
+    .attr("d", path)
+    .style("opacity", ".6")
+    .on("mouseenter", function (d) {
+      d3.select(this).classed("selected", true);
+      svg
+        .append("text")
+        .classed("tooltip", true)
+        .style("text-anchor", "middle")
+        .attr("x", 100)
+        .attr("y", 300)
+        .text(d.properties.name);
+    })
+    .on("mouseleave", function (d) {
+      d3.select(this).classed("selected", false);
+      d3.selectAll(".tooltip").remove();
+    });
+
+  enableRotation();
+
+  function drawGraticule() {
+    const graticule = d3.geoGraticule().step([10, 10]);
+
+    svg
+      .append("path")
+      .datum(graticule)
+      .attr("class", "graticule")
+      .attr("d", path)
+      .style("fill", "#fff")
+      .style("stroke", "#ccc");
+  }
+
+  function enableRotation() {
+    d3.timer(function (elapsed) {
+      projection.rotate([
+        config.speed * elapsed - 120,
+        config.verticalTilt,
+        config.horizontalTilt,
+      ]);
+      svg.selectAll("path").attr("d", path);
+    });
+  }
+}
+
+export { mapInit, globeInit };
--- src/components/worldMap/index.stories.js
+++ src/components/worldMap/index.stories.js
@@ -1,14 +1,14 @@
-
-import { withCssResources } from '@storybook/addon-cssresources';
-import init from './';
+import { withCssResources } from "@storybook/addon-cssresources";
+import { mapInit, globeInit } from "./";
 
 export default {
-  title: 'worldMap',
+  title: "worldMap",
   decorators: [withCssResources],
   parameters: {
-    cssresources: [{
-      id: `worldMap`,
-      code: `<style>
+    cssresources: [
+      {
+        id: "worldMap",
+        code: `<style>
       .area {
         stroke: gray;
         fill: #e5e5e5;
@@ -18,17 +18,26 @@ export default {
         fill: cadetblue;
       }
       </style>`,
-      picked: true,
-    },
+        picked: true,
+      },
     ],
   },
 };
 
-export const normal = () => {
-  const div = document.createElement('div');
+export const map = () => {
+  const div = document.createElement("div");
 
   // https://geojson-maps.ash.ms/
   const json = require("../../resources/custom.geo.json");
-  init(div, json.features);
+  mapInit(div, json.features);
+  return div;
+};
+
+export const globe = () => {
+  const div = document.createElement("div");
+
+  // https://geojson-maps.ash.ms/
+  const json = require("../../resources/custom.geo.json");
+  globeInit(div, json.features);
   return div;
 };
