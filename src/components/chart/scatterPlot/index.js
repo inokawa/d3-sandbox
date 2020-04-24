cc7ebbe20cc0196e4791ceaa7495515faa6fad32
n-inokawa <48897392+n-inokawa@users.noreply.github.com>
2020-04-24 16:05:39 +0900

    Refactor

--- src/components/chart/scatterPlot/index.js
+++ src/components/chart/scatterPlot/index.js
@@ -1,29 +1,28 @@
-import * as d3 from 'd3';
-import {setAxis} from '../axis';
+import * as d3 from "d3";
+import { setAxis } from "../axis";
 
 /**
  * 散布図
  * @param {*} elem
- * @param {*} xParam
- * @param {*} yParam
- * @param {*} name
+ * @param {*} param
  */
-function init(elem, xParam, yParam, name) {
+function init(elem, { xKey, yKey, xLabel, yLabel, name }) {
   // svg領域設定
   const svgWidth = 600;
   const svgHeight = 400;
-  const svg = d3.select(elem)
-    .append('svg')
-    .attr('width', svgWidth)
-    .attr('height', svgHeight);
+  const svg = d3
+    .select(elem)
+    .append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
   // グラフ領域設定
   const margin = { top: 50, right: 50, bottom: 50, left: 50 };
   const width = svgWidth - margin.right - margin.left;
   const height = svgHeight - margin.top - margin.bottom;
   const graph = svg
-    .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
   // scale設定
   const xScale = d3.scaleLinear().range([0, width]);
@@ -33,27 +32,26 @@ function init(elem, xParam, yParam, name) {
 
   function update(data) {
     // ドメイン設定
-    xScale.domain(d3.extent(data, d => d[xParam.key]));
-    yScale.domain([0, d3.max(data, d => d[yParam.key])]);
+    xScale.domain(d3.extent(data, (d) => d[xKey]));
+    yScale.domain([0, d3.max(data, (d) => d[yKey])]);
 
     // 軸の作成
-    setAxis(graph, xScale, yScale, xParam.name, yParam.name, width, height, margin);
+    setAxis(graph, xScale, yScale, xLabel, yLabel, width, height, margin);
 
     // 散布図の作成
-    const svgData = graph.selectAll('circle')
-      .data(data, d => d[name]);
+    const svgData = graph.selectAll("circle").data(data, (d) => d[name]);
 
-    svgData.enter()
-      .append('circle')
+    svgData
+      .enter()
+      .append("circle")
       .merge(svgData)
       .transition()
-      .attr('r', 3)
-      .attr('cx', d => xScale(d[xParam.key]))
-      .attr('cy', d => yScale(d[yParam.key]))
-      .attr('class', 'plot');
+      .attr("r", 3)
+      .attr("cx", (d) => xScale(d[xKey]))
+      .attr("cy", (d) => yScale(d[yKey]))
+      .attr("class", "plot");
 
-    svgData.exit()
-      .remove();
+    svgData.exit().remove();
   }
 }
 
--- src/components/chart/scatterPlot/index.stories.js
+++ src/components/chart/scatterPlot/index.stories.js
@@ -1,40 +1,38 @@
-
-import { withCssResources } from '@storybook/addon-cssresources';
-import init from '.';
-import { getRandom2dData } from '../randamDataCreator';
+import { withCssResources } from "@storybook/addon-cssresources";
+import init from ".";
+import { getRandom2dData } from "../randamDataCreator";
 
 export default {
-  title: 'chart/scatterPlot',
+  title: "chart/scatterPlot",
   decorators: [withCssResources],
   parameters: {
-    cssresources: [{
-      id: `scatterPlot`,
-      code: `<style>
+    cssresources: [
+      {
+        id: `scatterPlot`,
+        code: `<style>
       .plot {
         fill: steelblue;
       }
       </style>`,
-      picked: true,
-    },
+        picked: true,
+      },
     ],
   },
 };
 
 export const normal = () => {
-  const div = document.createElement('div');
-  const update = init(div,
-    {
-      key: 'x',
-      name: '名前'
-    },
-    {
-      key: 'y',
-      name: '値'
-    }, 'name');
+  const div = document.createElement("div");
+  const update = init(div, {
+    xKey: "x",
+    xLabel: "名前",
+    yKey: "y",
+    yLabel: "値",
+    name: "name",
+  });
 
   update(getRandom2dData(100, 1000));
   setInterval(() => {
     update(getRandom2dData(100, 1000));
-  }, 1000)
+  }, 1000);
   return div;
 };
